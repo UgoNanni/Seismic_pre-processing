68cb464f0312239d52978976c6be487db50b8d45
UgoNanni <42870130+UgoNanni@users.noreply.github.com>
2018-08-31 16:50:18 +0200

    Update peak_correct.sh

--- peak_correct.sh
+++ peak_correct.sh
@@ -8,8 +8,8 @@
 # Define path to data
 path2dat=/media/ugonanni/Nanni/DATA_ARG_backup/ARG_Borehole_Ugo/
 path2mseed=$path2dat/MSEED
-path2sac=$path2dat/SAC/SAC_raw
-path2sacpro=$path2dat/SAC/SAC_corrected
+path2sacsp=$path2dat/SAC/SAC_spikes
+path2sacclean=$path2dat/SAC/SAC_cleaned
 
 # Sac files with bleach event are characterized by starting after 03 secondes
 name_sp=003 # files with the spike
@@ -20,11 +20,30 @@ name_mer=mer # merged files
 # ========================================
 # Step 1: search for all files with '03.SAC'
 
-find *$name_sp*.SAC > spike_files_list.txt
+find -name *$name_sp*.SAC > spike_files_list.txt
 
 # loop over the file list
 for file in `cat spike_files_list.txt`
+do
   echo $file
+  new_file=`echo $file | sed -e 's#3.SAC#0.SAC#g'
+  cp $new_file $path2sacsp
+
+sac <<EOF
+r $file $new_file
+merge overlap average gap interp
+w tmp.SAC
+cut B 0 575
+r tmp.SAC
+cut off
+w $new_file
+quit
+EOF
+
+  mv $file $path2sacsp
+
+done
+
 
 
 
